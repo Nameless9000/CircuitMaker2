d1708611edd4536cd6b35c060ea9515979225943
Nameless 9000 <dev@name1ess.ga>
2024-04-29 22:52:53 +0100

    Added new nodes to NodeData

--- cm2cpp/data.h
+++ cm2cpp/data.h
@@ -4,6 +4,8 @@
 
 struct NodeData {
 	std::vector<Node> nodes;
+	std::vector<SpecialBuildingNode> special_buildings;
+	std::vector<SignNode> signs;
 
 	std::string compile(bool no_debug = false, bool compile_for_speed = false, bool optimize_positions = false, short max_x = 5, short max_z = 5);
 
